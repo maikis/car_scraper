204d5c37e8afdc6c8051a1988e823792072fa80a
Mykolas <mykolas@grineviciai.eu>
2017-02-05 14:48:29 +0200

    Refactor translator

--- lib/car_stalker/translator.rb
+++ lib/car_stalker/translator.rb
@@ -7,14 +7,24 @@ module CarStalker
       def translate(car_specs)
         translated_specs = {}
         car_specs.each do |spec, value|
-          transl_data = CarStalker::TRANSLATION_CONFIG.fetch(spec.to_sym)
-          transl_data.each do |site, mapping|
-            field = mapping.fetch(:field)
-            if translated_specs.key?(site)
-              translated_specs[site].merge!(:"#{field}" => value)
-            else
-              translated_specs[site] = { :"#{field}" => value }
-            end
+          translated_specs = translate_spec(spec, value, translated_specs)
+        end
+        translated_specs
+      end
+
+      private
+
+      def car_spec_data(spec)
+        CarStalker::TRANSLATION_CONFIG.fetch(spec.to_sym)
+      end
+
+      def translate_spec(spec, value, translated_specs)
+        car_spec_data(spec).each do |site, mapping|
+          field = mapping.fetch(:field)
+          if translated_specs.key?(site)
+            translated_specs[site].merge!(:"#{field}" => value)
+          else
+            translated_specs[site] = { :"#{field}" => value }
           end
         end
         translated_specs
