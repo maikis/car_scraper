01c19a045065fd10137da11f5a30658be59998ca
Mykolas Grinevicius <mykolas@grineviciai.eu>
2017-04-28 14:46:44 +0300

    Better format of autogidas model fetcher

--- lib/car_stalker/scripts/autogidas_model_fetcher.rb
+++ lib/car_stalker/scripts/autogidas_model_fetcher.rb
@@ -1,18 +1,37 @@
+#
+#
+# This get's all the models for each make. This is a lot of traffic,
+# so please use with care only if it's necessary in respect of Autogidas.
+#
+#
+
 require 'net/http'
 require 'json'
 require_relative '../translation_config'
 
-makes = CarStalker::TRANSLATION_CONFIG[:make][:autogidas][:options]
-
-result = []
-
-makes.map do |make|
-	uri = URI("https://autogidas.lt/ajax/category/models?category_id=01&make=#{make}")
-	response = Net::HTTP.get(uri)
-	puts response
-	models = JSON.parse(response)['data']
-	result << { "#{make}": models }
-	sleep(2)
+def convert_result(raw_models)
+  models = []
+  raw_models.map do |model|
+    title = model['title']
+    next if title.include?('Visi') || title.include?('kitas')
+    models << title
+  end
+  models
 end
 
-File.open('lib/car_stalker/scripts/autogidas_models.rb', 'w') { |file| file.write(result) }
+makes = CarStalker::TRANSLATION_CONFIG[:make][:autogidas][:options]
+result = {}
+
+makes.map do |make|
+  uri = URI('https://autogidas.lt/ajax/category/models?category_id='\
+            "01&make=#{make}")
+  response = Net::HTTP.get(uri)
+  models = JSON.parse(response)['data']
+  models = convert_result(models)
+  result[:"#{make}"] = models
+  sleep(2)
+end
+
+File.open('lib/car_stalker/data/autogidas_models.rb', 'w') do |file|
+  file.write("module CarStalker\n  AUTOGIDAS_MODELS = #{result}\nend")
+end
