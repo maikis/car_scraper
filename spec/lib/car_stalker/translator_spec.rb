b1fa7a5e3c6c11e38e785a4093b4cf20bbd123a8
Mykolas Grinevicius <mykolas@grineviciai.eu>
2017-05-08 16:13:02 +0300

    Re-write translator's specs

--- spec/lib/car_stalker/translator_spec.rb
+++ spec/lib/car_stalker/translator_spec.rb
@@ -1,29 +1,43 @@
 require 'spec_helper'
 
 describe CarStalker::Translator do
-  it 'translates specs without options' do
-    specs = { make: 'Volkswagen',
-              model: 'Golf',
-              kilometrage_to: '70000',
-              year_from: 2012,
-              price_to: '10000' }
-    expect(CarStalker::Translator.translate(specs)[:autoplius])
-      .to eq({ make_id_list: 'Volkswagen',
-               make_id_sublist_portal: 'Golf',
-               kilometrage_to: '70000',
-               make_date_from: 2012,
-               sell_price_to: '10000' })
+  describe 'target specific translations' do
+    specs = { make: 'Volkswagen', model: 'Golf' }
+    
+    it 'translates specs for autoplius' do
+      expect(described_class.translate(specs)).to include(:autoplius)
+    end
+
+    it 'translates specs for autogidas' do
+      expect(described_class.translate(specs)).to include(:autogidas)
+    end
   end
 
-  it 'translates options' do
-    specs = { damaged: :no,
-              fuel_type: :petrol,
-              gearbox: :automatic,
-              body_type: :hatchback }
-    expect(CarStalker::Translator.translate(specs)[:autoplius])
-      .to eq(has_damaged_id: 'No damages',
-             fuel_id: 'Petrol',
-             gearbox_id: 'Automatic',
-             body_type_id: 'Hatchback')
+  describe 'autoplius' do
+    it 'translates specs using specified options' do
+      specs = { damaged: :no,
+                fuel_type: :petrol,
+                gearbox: :automatic,
+                body_type: :hatchback }
+      expect(described_class.translate(specs).fetch(:autoplius))
+        .to eq(has_damaged_id: 'No damages',
+               fuel_id: 'Petrol',
+               gearbox_id: 'Automatic',
+               body_type_id: 'Hatchback')
+    end
+  end
+
+  describe 'autogidas' do
+    it 'translates specs using specified options' do
+      specs = { damaged: :no,
+                fuel_type: :petrol,
+                gearbox: :automatic,
+                body_type: :hatchback }
+      expect(described_class.translate(specs).fetch(:autogidas))
+        .to eq(f_46: 'Be defektų',
+               f_2: 'Benzinas',
+               f_10: 'Automatinė',
+               f_3: 'Hečbekas')
+    end
   end
 end
