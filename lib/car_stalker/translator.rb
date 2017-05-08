5f4572cbadd03a4bdd9505c8d66b6443fe1962be
Mykolas Grinevicius <mykolas@grineviciai.eu>
2017-05-08 18:00:05 +0300

    Fix spec translator to handle new config

--- lib/car_stalker/translator.rb
+++ lib/car_stalker/translator.rb
@@ -4,7 +4,10 @@ module CarStalker
   # Converter from user friendly car_spec config to specific page config.
   class Translator
     class << self
+      attr_reader :car_specs
+
       def translate(car_specs)
+        @car_specs = car_specs
         translated_specs = {}
         car_specs.each do |spec, value|
           translated_specs = translate_spec(spec, value, translated_specs)
@@ -18,21 +21,29 @@ module CarStalker
         CarStalker::TRANSLATION_CONFIG.fetch(spec.to_sym)
       end
 
-      def handle_value(value, mapping)
-        if mapping.include?(:options)
-          return translate_value(value, mapping[:options])
+      def handle_value(spec, value, mapping)
+        if spec == :make
+          # TODO: Validate make
+          return value
+        elsif spec == :model
+          # TODO: Validate model
+          return value
+        end
+
+        if mapping.fetch(:options).is_a?(Hash)
+          return translate_value(value, mapping.fetch(:options))
         end
         value
       end
 
       def translate_spec(spec, value, translated_specs)
-        car_spec_data(spec).each do |site, mapping|
-          value = handle_value(value, mapping)
+        car_spec_data(spec).each do |web_site, mapping|
+          site_value = handle_value(spec, value, mapping)
           field = mapping.fetch(:field)
-          if translated_specs.key?(site)
-            translated_specs[site].merge!(:"#{field}" => value)
+          if translated_specs.key?(web_site)
+            translated_specs[web_site].merge!(:"#{field}" => site_value)
           else
-            translated_specs[site] = { :"#{field}" => value }
+            translated_specs[web_site] = { :"#{field}" => site_value }
           end
         end
         translated_specs
@@ -41,6 +52,10 @@ module CarStalker
       def translate_value(value, options)
         options.fetch(value.to_sym)
         # TODO: Raise error if option not found.
+      end
+
+      def validate_value(value, options)
+        options.include?(value)
       end
     end
   end
